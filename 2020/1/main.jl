107c78348f141abe7744b4b013a307741797a2a7
harryscholes <harryscholes@users.noreply.github.com>
2020-12-02 15:24:51 +0000

    Improve time complexity

--- 2020/1/main.jl
+++ 2020/1/main.jl
@@ -2,9 +2,13 @@ using DelimitedFiles, Combinatorics
 
 xs = readdlm(joinpath(@__DIR__, "input.txt"), Int)
 
+const target = 2020
+
+# Part one: O(n^2)
+# Part two: O(n^3)
 function f(xs, n)
     for ys in combinations(xs, n)
-        if sum(ys) == 2020
+        if sum(ys) == target
             return prod(ys)
         end
     end
@@ -15,3 +19,32 @@ f(xs, 2)
 
 # Part two
 f(xs, 3)
+
+# Part one: O(n)
+function g(xs)
+    s = Set(xs)
+    for x in xs
+        y = target - x
+        if y in s
+            return x * y
+        end
+    end
+end
+
+g(xs)
+
+# Part two: O(n)
+function h(xs)
+    s = Set(xs)
+    for x in xs
+        r = target - x
+        for y in 1:t÷2
+            z = r - y
+            if y in s && z in s
+                return x * y * z
+            end
+        end
+    end
+end
+
+h(xs)
