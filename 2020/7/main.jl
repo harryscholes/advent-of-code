2abfe4c676d4f990218f88e852ebc20844fd3751
harryscholes <harryscholes@users.noreply.github.com>
2020-12-07 11:01:18 +0000

    Graphify

--- 2020/7/main.jl
+++ 2020/7/main.jl
@@ -5,27 +5,27 @@ const fp = "input.txt"
 const my_bag = "shiny gold"
 
 # Part one
-function inner_to_outer_relationships()
-    d = DefaultDict{String,Vector{String}}([])
+function graph()
+    graph = DefaultDict{String,Vector{String}}([])
     for policy in eachline(fp)
         occursin("no other bags", policy) && continue
         outer_bag = match(r"(\w+ \w+)", policy).captures[1]
         _, inner_bags = split(policy, "contain")
         for inner_bag in split(inner_bags, ",")
             inner_bag = match(r"(\d+) (\w+ \w+) bag", inner_bag).captures[2]
-            push!(d[inner_bag], outer_bag)
+            push!(graph[inner_bag], outer_bag)
         end
     end
-    return d
+    return graph
 end
 
-function find_bags(d)
+function bfs(graph)
     q = Queue{String}()
     enqueue!(q, my_bag)
     bags = Set{String}()
     while length(q) > 0
         inner_bag = dequeue!(q)
-        for outer_bag in d[inner_bag]
+        for outer_bag in graph[inner_bag]
             if outer_bag ∉ bags
                 enqueue!(q, outer_bag)
                 push!(bags, outer_bag)
@@ -35,8 +35,8 @@ function find_bags(d)
     return bags
 end
 
-d = inner_to_outer_relationships()
-length(find_bags(d))
+g = graph()
+length(bfs(g))
 
 # Part two
 struct Bag
@@ -44,8 +44,8 @@ struct Bag
     colour::String
 end
 
-function outer_to_inner_relationships()
-    d = DefaultDict{String,Vector{Bag}}([])
+function graph()
+    graph = DefaultDict{String,Vector{Bag}}([])
     for policy in eachline(fp)
         occursin("no other bags", policy) && continue
         outer_bag = match(r"(\w+ \w+)", policy).captures[1]
@@ -53,7 +53,7 @@ function outer_to_inner_relationships()
         for inner_bag in split(inner_bags, ",")
             m = match(r"(\d+) (\w+ \w+) bag", inner_bag)
             push!(
-                d[outer_bag],
+                graph[outer_bag],
                 Bag(
                     parse(Int, m.captures[1]),
                     m.captures[2],
@@ -61,16 +61,16 @@ function outer_to_inner_relationships()
             )
         end
     end
-    return d
+    return graph
 end
 
-function find_bags(d)
+function bfs(graph)
     q = Queue{Tuple{String,Int}}()
     enqueue!(q, (my_bag, 1))
     bags = Int[]
     while length(q) > 0
         outer_bag, n_outer = dequeue!(q)
-        for inner_bag in d[outer_bag]
+        for inner_bag in graph[outer_bag]
             n_inner = n_outer * inner_bag.amount
             enqueue!(q, (inner_bag.colour, n_inner))
             push!(bags, n_inner)
@@ -79,5 +79,5 @@ function find_bags(d)
     return bags
 end
 
-d = outer_to_inner_relationships()
-sum(find_bags(d))
+g = graph()
+sum(bfs(d))
