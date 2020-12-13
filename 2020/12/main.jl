a962e49f6082ab1062a4b1f5978b45b036da17ac
harryscholes <harryscholes@users.noreply.github.com>
2020-12-13 17:44:44 +0000

    Improved moving code

--- 2020/12/main.jl
+++ 2020/12/main.jl
@@ -10,11 +10,6 @@ const fp = "input.txt"
     F
 end
 
-struct Instruction
-    d::Direction
-    v::Int
-end
-
 const DIRECTION_MAPPER = Dict(
     "N" => N,
     "S" => S,
@@ -27,54 +22,59 @@ const DIRECTION_MAPPER = Dict(
 
 const INSTRUCTION_REGEX = r"^([A-Z])(\d+)$"
 
-instructions = map(eachline(fp)) do line
-    m = match(INSTRUCTION_REGEX, line)
-    Instruction(
-        DIRECTION_MAPPER[m.captures[1]],
-        mod(parse(Int, m.captures[2]), 360),
-    )
+struct Instruction
+    d::Direction
+    v::Int
 end
 
-manhattan(x, y) = abs(x) + abs(y)
+function Instruction(d::AbstractString, v::AbstractString)
+    Instruction(
+        DIRECTION_MAPPER[d],
+        mod(parse(Int, v), 360),
+    )
+end
 
 # Rotate 90° right
 rotate(x, y) = (y, -x)
 
+function move!(x, y, d, v)
+    if d == 0
+        y += v
+    elseif d == 90
+        x += v
+    elseif d == 180
+        y -= v
+    elseif d == 270
+        x -= v
+    end
+    return x, y
+end
+
+manhattan(x, y) = abs(x) + abs(y)
+
+instructions = map(eachline(fp)) do line
+    m = match(INSTRUCTION_REGEX, line)
+    Instruction(m.captures...)
+end
+
 # Part one
 
 function navigate(instructions; x, y, d)
-    prev_d = d
-
     for i in instructions
-        if i.d == R
-            d = mod(d + i.v, 360)
+        if i.d == L || i.d == R
+            v = i.v
+            if i.d == L
+                v *= -1
+            end
+            d = mod(d + v, 360)
             continue
-        elseif i.d == L
-            d = mod(d - i.v, 360)
-            continue
-        end
-
-        prev_d = d
-
-        if i.d == N || i.d == E || i.d == S || i.d == W
-            d = Int(i.d)
-            restore_d = true
-        end
-
-        if d == 0
-            y += i.v
-        elseif d == 90
-            x += i.v
-        elseif d == 180
-            y -= i.v
-        elseif d == 270
-            x -= i.v
-        else 
-            error("d = $d")
-        end
         
-        if i.d == N || i.d == E || i.d == S || i.d == W
-            d = prev_d
+        elseif i.d == N || i.d == E || i.d == S || i.d == W
+            temporary_d = Int(i.d)
+            x, y = move!(x, y, temporary_d, i.v)
+        
+        elseif i.d == F
+            x, y = move!(x, y, d, i.v)
         end
     end
 
@@ -88,16 +88,8 @@ manhattan(navigate(instructions; x=0, y=0, d=90)...)
 function navigate(instructions; x, y, wx, wy)
     for i in instructions
         if i.d == N || i.d == E || i.d == S || i.d == W
-            d = Int(i.d)
-            if d == 0
-                wy += i.v
-            elseif d == 90
-                wx += i.v
-            elseif d == 180
-                wy -= i.v
-            elseif d == 270
-                wx -= i.v
-            end
+            wx, wy = move!(wx, wy, Int(i.d), i.v)
+        
         elseif i.d == L || i.d == R
             angle = Int(i.v)
             # L is the inverse of R
@@ -109,6 +101,7 @@ function navigate(instructions; x, y, wx, wy)
             for _ in 1:n
                 wx, wy = rotate(wx, wy)
             end
+            
         elseif i.d == F
             x += wx * i.v
             y += wy * i.v
